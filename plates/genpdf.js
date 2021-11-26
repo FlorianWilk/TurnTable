9e8a6925c7ac9ba170e66d6d09f2c3cc6c24917c
florian.wilk <florian.wilk@gmail.com>
2021-11-26 13:06:45 +0100

    Noise on plate

--- plates/genpdf.js
+++ plates/genpdf.js
@@ -4,12 +4,29 @@ const SVGtoPDF = require('svg-to-pdfkit');
 // instantiate the library
 let doc = new PDFGenerator({size:"A4"})
 
-
 PDFGenerator.prototype.addSVG = function(svg, x, y, options) {
     return SVGtoPDF(this, svg, x, y, options), this;
   };
 
 doc.save();
+
+
+const sx=0;
+const sy=0;
+const dx=72*(20*0.393701);
+const dy=dx;
+
+for(i=0;i<4000;i++){
+    const xp=Math.random()*dx-dx/2;
+    const yp=Math.random()*dy-dy/2;
+    doc.rect(doc.page.width/2+xp,doc.page.height/2+yp, 10, 10).stroke();
+
+}
+
+
+
+
+
   doc.circle(doc.page.width/2, doc.page.height/2, 72*(20*0.393701) /2   )
   .dash(5, {space: 10})
   .stroke();
@@ -31,10 +48,16 @@ const p= 72*(11.5*0.393701)/2;
 const s=72;
 const s2=s/2;
 
-doc.image('CCTag/markersToPrint/generators/0000.png', doc.page.width/2-p-s2, doc.page.height/2-p-s2, {fit: [s, s], align: 'center', valign: 'center'})
-doc.image('CCTag/markersToPrint/generators/0001.png', doc.page.width/2+p-s2, doc.page.height/2-p-s2, {fit: [s, s], align: 'center', valign: 'center'})
-doc.image('CCTag/markersToPrint/generators/0002.png', doc.page.width/2+p-s2, doc.page.height/2+p-s2, {fit: [s, s], align: 'center', valign: 'center'})
-doc.image('CCTag/markersToPrint/generators/0003.png', doc.page.width/2-p-s2, doc.page.height/2+p-s2, {fit: [s, s], align: 'center', valign: 'center'})
+
+
+
+doc.image('CCTagOld/markersToPrint/generators/0000.png', doc.page.width/2-p-s2, doc.page.height/2-p-s2, {fit: [s, s], align: 'center', valign: 'center'})
+doc.image('CCTagOld/markersToPrint/generators/0001.png', doc.page.width/2+p-s2, doc.page.height/2-p-s2, {fit: [s, s], align: 'center', valign: 'center'})
+doc.image('CCTagOld/markersToPrint/generators/0002.png', doc.page.width/2+p-s2, doc.page.height/2+p-s2, {fit: [s, s], align: 'center', valign: 'center'})
+doc.image('CCTagOld/markersToPrint/generators/0003.png', doc.page.width/2-p-s2, doc.page.height/2+p-s2, {fit: [s, s], align: 'center', valign: 'center'})
+
+
+
 
 doc.pipe(fs.createWriteStream('TurnTablePlate_white.pdf'))
 
